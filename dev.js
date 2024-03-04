c3eb8877a0359e26897e9c525f75aa439d925cb6
Prathamesh <prathmesh@bigscal.com>
2024-03-04 11:51:27 +0530

    feature 2 added

--- dev.js
+++ dev.js
@@ -2,3 +2,4 @@ console.log("DEv branch code");
 console.log("pushed by another developer");
 console.log("Adding another feature");
 console.log("Adding another feature 1");
+console.log("Adding another feature 2");
